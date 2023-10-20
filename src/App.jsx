6fd9e2328e5dd2c936676b12d9340987d280ba7d
Jassherrin <jassherrin@gmail.com>
2023-10-20 06:55:44 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Jassherrin</h1>
+        <p>Software Engineer Trainee</p>
+      </header>
+      <section className="Portfolio">
+        <h2>Portfolio</h2>
+        {/* Add your projects here */}
+      </section>
+      <section className="About">
+        <h2>About Me</h2>
+        <p>Your bio and skills</p>
+      </section>
+      <section className="Contact">
+        <h2>Contact Me</h2>
+        <p>Email: your@email.com</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
